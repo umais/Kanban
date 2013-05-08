134deb7dea4e42a2ddd5f48dcfdb5f8ef3059631
Umais Siddiqui <umais20@yahoo.com>
2013-05-08 00:24:45 -0400

    Bootstrapped html with Angular

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -2,12 +2,24 @@
 
 /* Controllers */
 
-function PhoneListCtrl($scope) {
-    $scope.cards = { "Name": "Card1", "Content": "Testing Cards" };
-    
+function Boards($scope) {
+    $scope.name="This is a Board";
+    $scope.owner = "Umais Siddiqui" ;
+    $scope.active = "true";
+    $scope.lanes = [
+    {"name": "To Do",
+    "position": "0",
+    "cards": [{"name":"Card1","cardText":"Better do it fast","position":"0"}]
+    },
+     {
+         "name": "In Progress",
+         "position": "1",
+         "cards": [{ "name": "Card1", "cardText": "I am doing it", "position": "0" }]
+     },
+     {
+         "name": "Completed",
+         "position": "2",
+         "cards": [{ "name": "Card1", "cardText": "Faster than you think", "position": "0" }]
+     }
+  ];
 }
-
-
-
-
-
--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@
 
 
 $(function () {
-    createKanbanBoard();
+    //createKanbanBoard();
     initializeAlldroppers();
     $("#taskbtn").click(function () {
 
@@ -95,40 +95,40 @@ function DeleteDefaultNode() { }
 /*
 This Function is the key function to create the board. Eventually this will read the database by passing the member ID to the database and getting the entire board object.
 */
-function createKanbanBoard()
-{
-    var kanbanRow = document.getElementById("BoardRow");
-    var myLanes = document.createElement("div");
-    //Hard coded the number of lanes but eventually will be coming from Board Object stored in DB.
-	for (i = 0; i < 5; i++)
-    {
-        //Setting Up The Lanes
-        myLanes = document.createElement("div");
-        myLanes.setAttribute("id", "BoardLane" + i);
-        myLanes.setAttribute("class", "mycell");
-        //Setting Up the Lane Headers
-        var laneHeading = document.createElement("h1");
-        var laneHeadingText = document.createTextNode("Label " + i);
-        laneHeading.appendChild(laneHeadingText);
-        //Setting Up the DragBox. For each DragBox div there will also be a another Display Div That will be relative to this div for viewing details of the card.
-        var myDragBox = document.createElement("div");
-        myDragBox.setAttribute("class", "DragBox");
-        //setting up the dropbox List
-        var oList = document.createElement("ol");
-        oList.setAttribute("class", "dropbox");
-        var listItem = document.createElement("li");
-        listItem.setAttribute("id", "toDropDefault"+i);
-		//This will depend on the Board Object if there are already items then this default will not be created instead another loop will be needed here.
-        var listItemText = document.createTextNode("Item Will be created here. Currently No items!");
-        listItem.appendChild(listItemText);
-        oList.appendChild(listItem);
-        myDragBox.appendChild(oList);
+//function createKanbanBoard()
+//{
+//    var kanbanRow = document.getElementById("BoardRow");
+//    var myLanes = document.createElement("div");
+//    //Hard coded the number of lanes but eventually will be coming from Board Object stored in DB.
+//	for (i = 0; i < 5; i++)
+//    {
+//        //Setting Up The Lanes
+//        myLanes = document.createElement("div");
+//        myLanes.setAttribute("id", "BoardLane" + i);
+//        myLanes.setAttribute("class", "mycell");
+//        //Setting Up the Lane Headers
+//        var laneHeading = document.createElement("h1");
+//        var laneHeadingText = document.createTextNode("Label " + i);
+//        laneHeading.appendChild(laneHeadingText);
+//        //Setting Up the DragBox. For each DragBox div there will also be a another Display Div That will be relative to this div for viewing details of the card.
+//        var myDragBox = document.createElement("div");
+//        myDragBox.setAttribute("class", "DragBox");
+//        //setting up the dropbox List
+//        var oList = document.createElement("ol");
+//        oList.setAttribute("class", "dropbox");
+//        var listItem = document.createElement("li");
+//        listItem.setAttribute("id", "toDropDefault"+i);
+//		//This will depend on the Board Object if there are already items then this default will not be created instead another loop will be needed here.
+//        var listItemText = document.createTextNode("Item Will be created here. Currently No items!");
+//        listItem.appendChild(listItemText);
+//        oList.appendChild(listItem);
+//        myDragBox.appendChild(oList);
 
-        myLanes.appendChild(laneHeading);
-        myLanes.appendChild(myDragBox);
-        kanbanRow.appendChild(myLanes);
-    }
+//        myLanes.appendChild(laneHeading);
+//        myLanes.appendChild(myDragBox);
+//        kanbanRow.appendChild(myLanes);
+//    }
    
   
 
-}
+//}
